264f88bacf80c265355d2b8e42da9d2e0a50bd6f
Abdullah Senan Mahmod <abodsinan@github.com>
2018-11-19 23:44:51 +0800

    Added a kalman filter with some additions (noises, etc), next step will be implementing in simulink

--- MCS_proj.m
+++ MCS_proj.m
@@ -1,3 +1,5 @@
+%initialise the parameters (derivations will be shown elsewhere)
+% the states include euler angles and displacement
 A = [-0.0675 0 0 0 2.3694 0;
     0 -0.0675 0 0 0 -2.3694;
     1 0 0 0 0 0;
@@ -10,6 +12,23 @@ C = [1 0 0 0 0 0;
     329.3374 0 -9.1181 0 -11.8469 0;
     0 -392.3374 0 9.1181 0 -11.8469]
 D = [0 0; 0 0; 20.4512 0; 0 20.4512]
+%Turn into a state-space representation
 sys = ss(A, B, C, D)
+%check if the system is ctrb/obsv
 rank(ctrb(sys))
 rank(obsv(sys))
+%make a Q matrix(weights are all unity)
+Q = eye(6)
+% make an R matrix (weights are high because fuel is important)
+R = 100*eye(2)
+%obtain the gain matrix using LQR
+K = lqr(sys, Q, R)
+%Initialise some disturbances (will be better when implemented in simulink)
+Vd = .001* eye(6);
+Vn = .001;
+
+%Obtain the gain fuction for the estimator
+[L, P, K] = lqe(sys, Vd, Vn);
+%obtain the state-space model of the kalman filter
+Kal = ss(A-L*C, [B L], eye(6), 0*[B L]);
+%The next step is to simulate with simulink
